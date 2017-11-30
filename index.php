a9ff73438ea7d63f271f7e0d1976e562251aff77
scarecrowll <31948835+scarecrowll@users.noreply.github.com>
2017-11-30 15:44:52 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<?php 
-	$conn = pg_connect("host=ec2-54-235-210-115.compute-1.amazonaws.com port=5432 dbname=d8bmrqm12r9qrm user=miylqcpjgodxbc password=fce6a36620c8520ca48be45814b2cc90385ad3ce5cc8b1b725f3256a2cdfc463");
-	
+
+<?php
+	$conn = pg_connect("host=ec2-54-221-221-153.compute-1.amazonaws.com port=5432 dbname=d61jmbep5stsgi user=uyxnzawchpicvm password=587724c21903603d0814932b32fb538e822499ab7df78c311009f60c37b11adc");
 	if (!$conn) 
 	{
   		echo "An error occurred.";
   		exit;
 	}
-	$result = pg_query($conn, "SELECT * FROM n ORDER BY age");
+	$result = pg_query($conn, "SELECT * FROM student ORDER BY age");
 	if (!$result) 
 	{
   		echo "An error occurred.";
@@ -14,6 +14,7 @@
 	}
 	while ($row = pg_fetch_row($result)) 
 	{
-		echo"$row[0] $row[1] <br>";
+		echo"$row[0] $row[1] $row[2]<br>";
 	}
-	?>
+?>
+
